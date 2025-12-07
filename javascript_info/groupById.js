9029e7598654738e6525f0635ba0071ec38412b3
Samir Ibrahim <samir.ibrahim@outlook.it>
2025-12-07 17:10:30 +0100

    Complete 'javascript.info' objects exercises

---
+++ javascript_info/groupById.js
@@ -0,0 +1,27 @@
+function groupById(users) {
+  usersObj = {};
+  users.map((user) => {
+    usersObj[user.id] = { id: user.id, name: user.name, age: user.age };
+  });
+  return usersObj;
+}
+
+let users = [
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
+];
+
+let usersById = groupById(users);
+
+console.log(usersById);
+
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
