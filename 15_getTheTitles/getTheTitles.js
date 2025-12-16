09e65a71f44d03ccf8ca5fa21e816f1586e0ee3a
Samir Ibrahim <samir.ibrahim@outlook.it>
2025-12-16 06:59:06 +0100

    Complete javascript 'objects' repo exercises 15-16

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  return books.map((book) => book.title);
 };
 
+const books = [
+  {
+    title: "Il codice da vinci",
+    author: "Dan Brown",
+  },
+  {
+    title: "I Promessi Sposi",
+    author: "Alessandro Manzoni",
+  },
+];
+
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,42 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestPerson;
+  let maxAge = 0;
+  let age;
+  for (person of people) {
+    if (person.yearOfDeath !== undefined) {
+      age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      const currentTime = new Date().getFullYear();
+      age = currentTime - person.yearOfBirth;
+    }
 
+    if (age > maxAge) {
+      maxAge = age;
+      oldestPerson = person;
+    }
+  }
+  return oldestPerson;
 };
+
+// const people = [
+//   {
+//     name: "Carly",
+//     yearOfBirth: 1942,
+//     yearOfDeath: 1970,
+//   },
+//   {
+//     name: "Ray",
+//     yearOfBirth: 1962,
+//     yearOfDeath: 2011,
+//   },
+//   {
+//     name: "Jane",
+//     yearOfBirth: 1912,
+//     yearOfDeath: 1941,
+//   },
+// ];
+
+//console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
